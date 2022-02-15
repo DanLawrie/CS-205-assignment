e573b3ba0ac637b2c48c95a56c3148bf84514f7d
DanLawrie <71726127+DanLawrie@users.noreply.github.com>
2022-02-14 21:05:30 -0500

    Add files via upload

--- Animals/Fish.java
+++ Animals/Fish.java
@@ -2,18 +2,19 @@ package edu.monmouth.Animals;
 
 public class Fish implements Animal{
 	protected String color = "";
-	private static final String fishNoise = "bwah";
-	private Byte move = 0;
+	private static final String fishNoise = "bubble";
 	
+	/*
+	 * accepts a String and sets color to that String
+	 */
 	Fish(String color){
 		setColor(color);
 	}
 	
-	//prints out how far the fish has moved
+	//prints out how the fish has moved as a String
 	@Override
 	public void move() {
-		move++;
-		System.out.println("The fish has moved: "+ move);
+		System.out.println("The fish has moved");
 	}
 	
 	
